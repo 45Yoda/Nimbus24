d15655f164eaa557512cf14fd9fc6ad41ed7bfa0
elpaquito30 <filipe.v.f@hotmail.com>
2018-06-09 22:41:32 +0100

    asd

---
+++ Nimbus24/Models/Cidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nimbus24.Models
+{
+    public partial class Cidade
+    {
+        public Cidade()
+        {
+            Morada = new HashSet<Morada>();
+            Prestador = new HashSet<Prestador>();
+        }
+
+        public string Cidade1 { get; set; }
+
+        public ICollection<Morada> Morada { get; set; }
+        public ICollection<Prestador> Prestador { get; set; }
+    }
+}
---
+++ Nimbus24/Models/TipoServico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nimbus24.Models
+{
+    public partial class TipoServico
+    {
+        public string Id { get; set; }
+        public int? Tipo { get; set; }
+        public decimal? Preco { get; set; }
+        public string IdPrestador { get; set; }
+        public int? Negociavel { get; set; }
+
+        public Prestador IdPrestadorNavigation { get; set; }
+    }
+}
